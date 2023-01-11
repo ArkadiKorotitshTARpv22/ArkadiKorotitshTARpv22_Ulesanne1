dbe6037cd6a750d173c09b963b3eed41692b1685
opilane <opilane@HP-CZC2349HT9>
2023-01-11 12:29:51 +0200

    Add project files.

---
+++ ArkadiKorotitshTARpv22_3.py
@@ -0,0 +1,60 @@
+﻿#1 Ülesanne 1 kasutades "While"
+#print("---------------------------------------------------------")#eraldamine
+#N=input("Palun sisesta oma nimi: ")
+#k=float(input("Mitu korda soovite, et teid tervitataks? "))
+#x=1
+#while x < k+1:
+#    print("Ole tervitatud,",N,x,".korda.")
+#    x=x+1
+
+##2 Ülesanne 1 kasutades "While True"
+#print("---------------------------------------------------------")
+#M=input("Palun sisesta oma nimi: ")
+#l=float(input("Mitu korda soovite, et teid tervitataks? "))
+#z=1
+#while True:
+#    print("Ole tervitatud,",M,z,".korda.")
+#    z=z+1
+#    if z==l+1: 
+#        break
+
+##3 Ülesanne 1 kasutades "For"
+#print("---------------------------------------------------------")
+#B=input("Palun sisesta oma nimi: ")
+#j=float(input("Mitu korda soovite, et teid tervitataks? "))
+#y=j+1
+#for i in range(int(y)):
+#    print("Ole tervitatud,",B,i,".korda.")
+
+##4 Ülesanne 1 kasutades "While" ja "For"
+#print("---------------------------------------------------------")
+#V=input("Palun sisesta oma nimi: ")
+#f=float(input("Mitu korda soovite, et teid tervitataks? "))
+#t=f+1
+#c=1
+#while c in range(int(t)):
+#    print("Ole tervitatud,",V,c,".korda.")
+#    c +=1
+
+# Ülesanne 10
+#x=1
+#while x<=100:
+#    if x%5==0:
+#        print(x, end=" ")
+#    x+=1
+# Ülesanne ?
+#try:
+#num_horiz-int(input("Sisesta ruutude arv horisontaalselt =>> \n"))
+#print("Value Error")
+#num_horiz=randint(1, 20) I
+#except:
+#try:
+#num_vert=int (input("Sisesta
+#print("Value Error")
+#num_vert=randint(1, 20)
+#except:
+#for y in range(num_vert):
+#for x in range(num_horiz):
+#print("o", end=" ")
+#print ()
+#ruutude arv vertikalselt =>> \n"))
